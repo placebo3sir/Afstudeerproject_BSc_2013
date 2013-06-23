40d58a88f697c8d2b1cbf1bd7e7a6f6607aa6b25
plcabo3sir <placebo3sir@gmail.com>
2013-06-23 12:32:27 +0200

    Version1

--- coat_detection/hog_features_abstraction.m
+++ coat_detection/hog_features_abstraction.m
@@ -5,20 +5,17 @@ run /Users/Capodit3/Downloads/vlfeat-0.9.16/toolbox/vl_setup;
 % --------------------------------------------------------------------
 %                                                 Compute HOG features
 % --------------------------------------------------------------------
+
+im2 = '493612cb9b9cfd6b20e01d91b05b6a8177187f314ad1a916e0a7fb99ea974878.jpg';
+
 im = imread(fullfile(vl_root,'data',strcat('coa/', im))) ;
 im = im2single(im) ;
 
-% A(:,:,1) = ones(2,2);
-% A(:,:,2) = 2*ones(2,2);
-% A(:,:,3) = 3*ones(2,2);
-% 
-% im = permute(A,[1 3 2]);
-% im = reshape(im,[],size(A,2),1);
 
 % Standard features
 cellSize = 8 ;
-hog = vl_hog(im, cellSize, 'verbose', 'numOrientations', 8) ;
-imhog = vl_hog('render', hog, 'verbose', 'numOrientations', 8) ;
+hog = vl_hog(im, cellSize, 'verbose', 'numOrientations', 3) ;
+imhog = vl_hog('render', hog, 'verbose', 'numOrientations', 3) ;
 
 clf ; imagesc(imhog) ; colormap gray ;
 
@@ -29,27 +26,22 @@ bw = im2bw(imhog, graythresh(imhog));
 bw2 = imfill(bw, 'holes');
 bw2 = bw2 >= 0.9;
 L = bwlabel(bw2);
-% imshow(bw>=1);
-% subplot(1,2,2) ;
-% [r, c] = find(imhog>=0.9);
-% rc = [r c];
 
 stats = regionprops(L,{'BoundingBox', 'Centroid'});
 hold on
 figure(11);
-imagesc(bw);axis image off ; colormap gray ;
-size(bw2);
+imagesc(im);axis image off ; colormap gray ;
 title('Testing');
 for object = 1 : length(stats)
     bb = stats(object).BoundingBox;
-    if (( bb(:,1) > 100 && bb(:,1) < 200 ||... % left
-            bb(:, 1) > 200 && bb(:, 1) < 700 ||... % middle
-            bb(:, 1) > 700  )&&... % right
-            bb(:,3) > 150 && bb(:,4) > 80 && bb(:,3) < 500) 
-    rectangle('Position',bb,'EdgeColor','g', 'Curvature',[0.8,0.4], ...
-    'LineWidth',2)
-    end
-    
+%     if (( bb(:,1) > 100 && bb(:,1) < 200 ||... % left
+%             bb(:, 1) > 200 && bb(:, 1) < 700 ||... % middle
+%             bb(:, 1) > 700  )&&... % right
+% %             bb(:,3) > 150 && bb(:,4) > 80 && bb(:,3) < 500) 
+%     rectangle('Position',bb,'EdgeColor','g', 'Curvature',[0.8,0.4], ...
+%     'LineWidth',2)
+%     end
+  showboxes(im, bb)  ;
     if(bb(:, 1) > 200 && bb(:, 1) < 700 &&...
             bb(:,3) > 100 && bb(:,4) > 80)... % middle
         
@@ -58,6 +50,36 @@ for object = 1 : length(stats)
     end
 end
 hold off
+
+
+
+
+im2 = imread(fullfile(vl_root,'data',strcat('coa/', im2))) ;
+im2 = im2single(im2) ;
+
+% Standard features
+cellSize = 8 ;
+hog2 = vl_hog(im2, cellSize, 'verbose', 'numOrientations', 3) ;
+imhog2 = vl_hog('render', hog2, 'verbose', 'numOrientations', 3) ;
+
+clf ; imagesc(imhog2) ; colormap gray ;
+
+
+% im = im(1:128,end-128+1:end,:) ;
+
+bwb = im2bw(imhog2, graythresh(imhog2));
+bw2b = imfill(bwb, 'holes');
+bw2b = bw2b >= 0.9;
+L = bwlabel(bw2b);
+
+i = 'Entering comparing...'
+% apply templete matching using power of the image
+result1=tmp(bw,bwb);
+i = 'Finished comparing...'
+figure,
+subplot(2,2,1),imshow(bw);title('Template');
+subplot(2,2,2),imshow(bwb);title('Target');
+subplot(2,2,3),imshow(result1);title('Matching Result using tmp');
 
 % Get permutation to flip a HOG cell from left to right
 % --------------------------------------------------------------------
